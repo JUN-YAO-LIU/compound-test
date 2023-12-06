5accf6936accb6b153adf4239d40733f35d788f1
劉駿嶢 <liujunyao@liujunyaodeMacBook-Pro.local>
2023-12-06 21:13:43 +0800

    aave

--- test/CompoundPracticeHW3.t.sol
+++ test/CompoundPracticeHW3.t.sol
@@ -8,6 +8,8 @@ import "test/helper/CompoundInitSetUp.sol";
 import { CToken } from "compound-protocol/contracts/CToken.sol";
 import { Comptroller } from "compound-protocol/contracts/Comptroller.sol";
 import { Unitroller } from "compound-protocol/contracts/Unitroller.sol";
+import "../constracts/Uni/ISwapRouter.sol";
+import { Pool } from "../lib/aave-address-book/lib/aave-v3-core/contracts/protocol/pool/Pool.sol";
 
 contract CompoundPracticeTestHW3 is Test {
   EIP20Interface public JT = EIP20Interface(0xbd098C26334CA2E2690d14D59E8D4d623241D7F1);
@@ -26,6 +28,8 @@ contract CompoundPracticeTestHW3 is Test {
   address public oracleAdmin = 0xCD8f976a4f5Ba1f577adf7666EA2C5389D3cCD53;
   SimplePriceOracle oracle;
 
+  Pool pool = Pool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
+
   function setUp() public {
     uint256 forkId = vm.createFork(
             "https://eth-mainnet.g.alchemy.com/v2/k-sz4T_Vr7gOvMk-OHpUTzlAiU9VDs3q"
@@ -42,6 +46,10 @@ contract CompoundPracticeTestHW3 is Test {
     oracle.setUnderlyingPrice(CToken(address(cUSNI)),5 * 1e18);
     Comptroller(address(unitroller))._setCollateralFactor(CToken(address(cUSNI)),25 * 1e17);
     vm.stopPrank();
+
+    vm.label(address(pool), "FlashPool");
+    vm.label(user1, "User1");
+    vm.label(user2, "User2");
   }
 
   function test_AAVE_Flashloan_Liquidate() public {
@@ -62,12 +70,55 @@ contract CompoundPracticeTestHW3 is Test {
 
     Comptroller(address(unitroller)).enterMarkets(tokens);
 
-    // 不能用1e18的寫法來運算
     cUSDC.borrow((2500 * 10 ** TokenA_USDC.decimals()));
     console2.log("user1 USDC:",TokenA_USDC.balanceOf(user1));
+    vm.stopPrank();
 
     vm.startPrank(admin);
     oracle.setUnderlyingPrice(CToken(address(cUSNI)),4 * 1e18);
     vm.stopPrank();
+
+    vm.startPrank(user2);
+    uint closeFactorMantissa = Comptroller(address(unitroller)).closeFactorMantissa();
+    uint borowBalance = cUSDC.borrowBalanceCurrent(user1);
+    uint repayAmount = borowBalance * closeFactorMantissa / 1e18;
+    console2.log("repayAmount:",repayAmount);
+
+    // borrow what token, just takenOut what token.
+    // ISwapRouter.ExactInputSingleParams memory swapParams =
+    // ISwapRouter.ExactInputSingleParams({
+    //   tokenIn: UNI_ADDRESS,
+    //   tokenOut: USDC_ADDRESS,
+    //   fee: 3000, // 0.3%
+    //   recipient: address(this),
+    //   deadline: block.timestamp,
+    //   amountIn: uniAmount,
+    //   amountOutMinimum: 0,
+    //   sqrtPriceLimitX96: 0
+    // });
+
+    // swap Router = 0xE592427A0AEce92De3Edee1F18E0157C05861564
+    // uint256 amountOut = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564)
+    // .exactInputSingle(swapParams);
+
+
+    address[] memory assets = new address[](2);
+    assets[0] = address(TokenA_USDC);
+    // assets[1] = address(TokenB_UNI);
+
+    uint256[] memory amounts = new uint256[](2);
+    amounts[0] = repayAmount;
+
+    uint256[] memory modes = new uint256[](2);
+    modes[0] = 0;
+
+    // repay
+    pool.flashLoan(user2,assets,amounts,modes,address(this),new bytes(0),0);
+    console2.log("user2 USDC:",TokenA_USDC.balanceOf(user2));
+    
+    // console2.log("<<user1 account liquidity After change collateral factor>>");
+    // (,uint liquidityAfter,uint shortfallAfter) = Comptroller(address(unitroller)).getAccountLiquidity(user1);
+    // console2.log("user1 liquidity:",liquidityAfter);
+    // console2.log("user1 shortfall:",shortfallAfter);
   }
 }
